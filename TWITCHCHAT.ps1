2f557697bb45beb0bb9b20cee591c86ad0bc2b24
MitchellV3 <30252114+MitchellV3@users.noreply.github.com>
2022-11-19 00:45:00 -0700

    Add more VOD & chat download options

--- TWITCHCHAT.ps1
+++ TWITCHCHAT.ps1
@@ -6,22 +6,38 @@ New-Item -ItemType Directory -Force -Path $args[4]
 $jsonPath = $args[0] + "-chat.json" -replace "'", ""
 $renderPath = $args[1] + "-chat_render.mp4" -replace "'", ""
 $vodId = $args[2].Split("/")[-1]
+$downloadType = $args[5]
 
 Write-Output ""
 Write-Output $vodId
+#Write-Output $args[0]
+#Write-Output $args[1]
+#Write-Output $args[3]
+#Write-Output $args[4]
+#Write-Output $jsonPath
+#Write-Output $renderPath
 
-TwitchDownloaderCLI `
-    -m ChatDownload `
-    --embed-emotes `
-    --id $vodId `
-    -o "$jsonPath" `
-    --threads 4
-TwitchDownloaderCLI `
-    -m ChatRender `
-    -i $jsonPath `
-    -h 1080 -w 422 `
-    --font-size 18 `
-    -o $renderPath `
-    --threads 4
+if ($downloadType -eq "1" -or $downloadType -eq "4") {
+    TwitchDownloaderCLI `
+        -m ChatDownload `
+        --embed-emotes `
+        --id $vodId `
+        -o "$jsonPath" `
+        --threads 4
+    TwitchDownloaderCLI `
+        -m ChatRender `
+        -i "$jsonPath" `
+        -h 1080 -w 422 `
+        --font-size 18 `
+        -o "$renderPath" `
+        --threads 4
+} elseif ($downloadType -eq "2") {
+    TwitchDownloaderCLI `
+        -m ChatDownload `
+        --embed-emotes `
+        --id $vodId `
+        -o "$jsonPath" `
+        --threads 4
+}
 
 PowerShell -NoExit
--- TWITCHVOD.ps1
+++ TWITCHVOD.ps1
@@ -1,4 +1,13 @@
 Write-Output "----------Twitch VOD----------"
+Write-Output ""
+Write-Output "***Download Type***"
+Write-Output "1 - VOD + Chat Render (JSON + MP4)"
+Write-Output "2 - VOD + Chat Archive (JSON)"
+Write-Output "3 - VOD Only"
+Write-Output "4 - Chat Render Only"
+Write-Output ""
+$downloadType = Read-Host -Prompt "Select Download Type"
+
 $url = Read-Host -Prompt "Enter URL"
 $url = $url.Split("?")[0]
 Write-Output $url
@@ -14,21 +23,58 @@ $RenderPath = $homePath + $videoPath + "/[Chat]"
 $chatJson = $JsonPath + "/(%(upload_date>%Y-%m-%d)s) %(title)s [%(id)s]" 
 $chatRender = $RenderPath + "/(%(upload_date>%Y-%m-%d)s) %(title)s [%(id)s]" 
 
-yt-dlp `
-    $url `
-    -f "bv+ba/b" `
-    -P "$homePath" `
-    -o "$videoPath/$videoNameScheme" `
-    --download-archive "$archivePath" `
-    --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json `
-    --replace-in-metadata "title" "\|", " " `
-    --replace-in-metadata "title" "'", ";" `
-    --write-info-json -o "infojson:$videoPath/[Archive]/Info/$videoNameScheme" `
-    --write-link -o "link:$videoPath/[Archive]/Links/$videoNameScheme" `
-    --write-description -o "description:$videoPath/[Archive]/Descriptions/$videoNameScheme" `
-    --write-thumbnail -o "thumbnail:$videoPath/[Archive]/Thumbnails/$videoNameScheme" `
-    --merge-output-format "mp4" `
-    --downloader "aria2c" `
-    --exec "after_video:powershell $chatDownloader '$chatJson' '$chatRender' $url '$JsonPath' '$RenderPath'" `
+
+if ($downloadType -eq "1" -or $downloadType -eq "2") {
+    yt-dlp `
+        $url `
+        -f "bv+ba/b" `
+        -P "$homePath" `
+        -o "$videoPath/$videoNameScheme" `
+        --download-archive "$archivePath" `
+        --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json `
+        --replace-in-metadata "title" "\|", " " `
+        --replace-in-metadata "title" "'", ";" `
+        --write-info-json -o "infojson:$videoPath/[Archive]/Info/$videoNameScheme" `
+        --write-link -o "link:$videoPath/[Archive]/Links/$videoNameScheme" `
+        --write-description -o "description:$videoPath/[Archive]/Descriptions/$videoNameScheme" `
+        --write-thumbnail -o "thumbnail:$videoPath/[Archive]/Thumbnails/$videoNameScheme" `
+        --merge-output-format "mp4" `
+        --downloader "aria2c" `
+        --exec "after_video:powershell $chatDownloader '$chatJson' '$chatRender' $url '$JsonPath' '$RenderPath' $downloadType" 
+} elseif ($downloadType -eq "3") {
+    yt-dlp `
+        $url `
+        -f "bv+ba/b" `
+        -P "$homePath" `
+        -o "$videoPath/$videoNameScheme" `
+        --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json `
+        --replace-in-metadata "title" "\|", " " `
+        --replace-in-metadata "title" "'", ";" `
+        --write-info-json -o "infojson:$videoPath/[Archive]/Info/$videoNameScheme" `
+        --write-link -o "link:$videoPath/[Archive]/Links/$videoNameScheme" `
+        --write-description -o "description:$videoPath/[Archive]/Descriptions/$videoNameScheme" `
+        --write-thumbnail -o "thumbnail:$videoPath/[Archive]/Thumbnails/$videoNameScheme" `
+        --downloader "aria2c"
+} elseif ($downloadType -eq "4") {
+    yt-dlp `
+        $url `
+        -f "bv+ba/b" `
+        -P "$homePath" `
+        -o "$videoPath/$videoNameScheme" `
+        --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json `
+        --replace-in-metadata "title" "\|", " " `
+        --replace-in-metadata "title" "'", ";" `
+        --replace-in-metadata "title" ":", ";" `
+        --write-info-json -o "infojson:$videoPath/[Archive]/Info/$videoNameScheme" `
+        --write-link -o "link:$videoPath/[Archive]/Links/$videoNameScheme" `
+        --write-description -o "description:$videoPath/[Archive]/Descriptions/$videoNameScheme" `
+        --write-thumbnail -o "thumbnail:$videoPath/[Archive]/Thumbnails/$videoNameScheme" `
+        --downloader "aria2c" `
+        --skip-download `
+        --exec "after_video:powershell $chatDownloader '$chatJson' '$chatRender' $url '$JsonPath' '$RenderPath' $downloadType" 
+} else {
+    Write-Output "Invalid Download Type"
+}
+
 
 PowerShell -NoExit
